0c12d23edcde07e179c1e6a3b74af26b0edb6c75
132ndNeck <neck.dcs@gmail.com>
2025-02-09 00:02:32 +0100

    chg: Added skynet for 1st Corps

---
+++ l10n/DEFAULT/skynet-iads-setup-OPAC_1st_corps.lua
@@ -0,0 +1,20 @@
+do
+--create an instance of the IADS
+redIADS = SkynetIADS:create('FIRSTCORPS')
+
+
+--add all units with unit name beginning with 'EWR' to the IADS:
+redIADS:addEarlyWarningRadarsByPrefix('FIRSTSBORKA')
+
+--add all groups begining with group name 'IADS' to the IADS:
+redIADS:addSAMSitesByPrefix('FIRSTAD')
+
+--add a command center:
+commandCenter = StaticObject.getByName('FIRSTCORPSCOMMANDCENTER')
+redIADS:addCommandCenter(commandCenter)
+
+
+--activate the IADS
+redIADS:activate()
+
+end

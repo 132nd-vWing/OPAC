19805acaae802bc069679cd11e06f4875cfc23ec
132ndNeck <neck.dcs@gmail.com>
2025-02-08 13:52:54 +0100

    chg: Added Moose and initial skynet for IADS

---
+++ l10n/DEFAULT/skynet-iads-setup-OPAC.lua
@@ -0,0 +1,116 @@
+do
+--create an instance of the IADS
+redIADS = SkynetIADS:create('NOTIA')
+
+
+
+--add all units with unit name beginning with 'EWR' to the IADS:
+redIADS:addEarlyWarningRadarsByPrefix('EWR')
+
+--add all groups begining with group name 'IADS' to the IADS:
+redIADS:addSAMSitesByPrefix('IADS')
+
+--add a command center:
+commandCenter = StaticObject.getByName('ADCC')
+redIADS:addCommandCenter(commandCenter)
+
+
+-- EWR CONNECTION NODES
+
+--EWR CONNECTION TO SECTOR COMMAND CENTER - NORTH
+--add a power source and a connection node for this EW radar:
+local connectionNodeEW = StaticObject.getByName('SCC NORTH')
+redIADS:getEarlyWarningRadarByUnitName('EWR_NORTH_1'):addConnectionNode(connectionNodeEW)
+
+----add a power source and a connection node for this EW radar :
+local connectionNodeEW = StaticObject.getByName('SCC NORTH')
+redIADS:getEarlyWarningRadarByUnitName('EWR_NORTH_2'):addConnectionNode(connectionNodeEW)
+
+----add a power source and a connection node for this EW radar:
+local connectionNodeEW = StaticObject.getByName('SCC NORTH')
+redIADS:getEarlyWarningRadarByUnitName('EWR_NORTH_3'):addConnectionNode(connectionNodeEW)
+
+local connectionNodeEW = StaticObject.getByName('SCC NORTH')
+redIADS:getEarlyWarningRadarByUnitName('EWR_NORTH_4'):addConnectionNode(connectionNodeEW)
+
+----add a power source and a connection node for this EW radar :
+local connectionNodeEW = StaticObject.getByName('SCC NORTH')
+redIADS:getEarlyWarningRadarByUnitName('EWR_NORTH_5'):addConnectionNode(connectionNodeEW)
+
+----add a power source and a connection node for this EW radar:
+local connectionNodeEW = StaticObject.getByName('SCC NORTH')
+redIADS:getEarlyWarningRadarByUnitName('EWR_NORTH_6'):addConnectionNode(connectionNodeEW)
+
+
+-- EWR CONNECTION TO SECTOR COMMAND CENTER - SOUTH
+--add a power source and a connection node for this EW radar:
+local connectionNodeEW = StaticObject.getByName('SCC SOUTH')
+redIADS:getEarlyWarningRadarByUnitName('EWR_SOUTH_1'):addConnectionNode(connectionNodeEW)
+
+----add a power source and a connection node for this EW radar:
+local connectionNodeEW = StaticObject.getByName('SCC SOUTH')
+redIADS:getEarlyWarningRadarByUnitName('EWR_SOUTH_2'):addConnectionNode(connectionNodeEW)
+
+----add a power source and a connection node for this EW radar:
+local connectionNodeEW = StaticObject.getByName('SCC SOUTH')
+redIADS:getEarlyWarningRadarByUnitName('EWR_SOUTH_3'):addConnectionNode(connectionNodeEW)
+
+
+
+
+-- POINT DEFENCE
+-- POINT DEFENCE SECTOR NORTH
+-- SA-2 Rybachy peninsula
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD1_SA15_1')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH _8011_SA2_BN'):addPointDefence(sa15)
+
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD1_SA15_2')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH _8011_SA2_BN'):addPointDefence(sa15)
+
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD1_SA15_3')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH _8011_SA2_BN'):addPointDefence(sa15)
+
+-- SA-5 
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD2_SA15_1')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH_8001_SA5_BN'):addPointDefence(sa15)
+
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD2_SA15_2')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH_8001_SA5_BN'):addPointDefence(sa15)
+
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD2_SA15_3')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH_8001_SA5_BN'):addPointDefence(sa15)
+
+
+-- SA-10 
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD3_SA15_1')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH_8291_SA10_BN'):addPointDefence(sa15)
+
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD3_SA15_2')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH_8291_SA10_BN'):addPointDefence(sa15)
+
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD3_SA15_3')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH_8291_SA10_BN'):addPointDefence(sa15)
+
+
+-- SA-2 ivo MURMANSK
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD4_SA15_1')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH_8111_SA2_BN'):addPointDefence(sa15)
+
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD4_SA15_2')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH_8111_SA2_BN'):addPointDefence(sa15)
+
+--local sa15 = redIADS:getSAMSiteByGroupName('IADS_NORTH_PD4_SA15_3')
+--redIADS:getSAMSiteByGroupName('IADS_NORTH_8111_SA2_BN'):addPointDefence(sa15)
+
+
+
+
+--activate the IADS
+redIADS:activate()
+
+--Moose integration
+DetectionSetGroup = SET_GROUP:New()
+redIADS:addMooseSetGroup(DetectionSetGroup)
+
+
+end
